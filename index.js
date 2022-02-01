88677d04d96d3b35bedef8041d900e69758b7ee0
Juan Padron Jr <jpadron@student.neumont.edu>
2022-01-31 20:49:54 -0700

    1-31-2022 Commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const exp = require('constants');
     path = require('path');
     routes = require('./routes/routes.js');
     app = express();
+    http = require('http').createServer(app);
+    io = require('socket.io')(http);
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -19,5 +21,28 @@ app.get('/', routes.index);
 app.get('/create', routes.create);
 app.post('/create', urlencodedParser, routes.createPerson);
 
+app.get('/join', function(req, res){
+    res.sendFile(process.cwd() + '/views/join.html');
+  });
+app.get('/start', function(req, res){
+    res.sendFile(process.cwd() + '/views/start.html');
+  });
 
-app.listen(3000)
+io.on('connection', (socket) => {
+  console.log('a new user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+  
+
+//app.listen(3000)
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+//https://docs.gitlab.com/ee/gitlab-basics/start-using-git.html
+//https://www.atlassian.com/git/tutorials/syncing/git-pull
+//https://stackoverflow.com/questions/60966525/nginx-how-to-run-server-js-file-not-html-always-in-some-port
